f693a2057ed8e47b4090d684b661a452847f2dde
obinna <obinna.aguwa@ceridian.com>
2023-06-01 20:39:32 +0400

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Obinna Aguwa on 01/06/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
