f14fe5a400d297dd60d279c6c5a7da61db570ec5
obinna <obinna.aguwa@ceridian.com>
2023-06-21 12:25:38 +0400

    Guarantee that we can load more than once.

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -31,6 +31,23 @@ class RemoteFeedLoaderTests: XCTestCase {
         // Then assert that a URL request was initiated in the client.
         XCTAssertEqual(client.requestedURL, url)
     }
+
+    func test_loadTwice_requestsDataFromURLTwice() {
+
+        // Arrange or Given
+        // "Given a url, a client and a sut"
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        // Act or When
+        // "When we invoke`sut.load()` twice"
+        sut.load()
+        sut.load()
+
+        // Assert or Then
+        // Then assert that a URL request was initiated in the client.
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
     
     // MARK: - Helpers
     
@@ -42,9 +59,11 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLs = [URL]()
         
         func get(from url: URL) {
             requestedURL = url
+            requestedURLs.append(url)
         }
     }
 }
