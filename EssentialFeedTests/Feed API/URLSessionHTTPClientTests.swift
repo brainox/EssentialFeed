5dae85f8793da75a7ab1c59b914813a0aa0a3c25
Obinna <macmartins081@gmail.com>
2023-09-09 10:42:43 +0400

    Added comments to URLSession data tasks test.

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -24,20 +24,31 @@ class URLSessionHTTPClent {
 class URLSessionHTTPClientTests: XCTestCase {
     
     func test_getFromURL_creaesDataTaskWithURL() {
+        // Arrange (setup)
         let url = URL(string: "http://any-url.com")!
         let session = URLSessionSpy()
         let sut = URLSessionHTTPClent(session: session)
-        
+
+        // Act
+        // `get` is invoked with the url.
         sut.get(from: url)
         
+        // Assert (Expectation)
+        // Expect SUT to create a dataTask from the session with the received URL
         XCTAssertEqual(session.receivedURLs, [url])
     }
     
     // MARK: - Helpers
     
     private class URLSessionSpy: URLSession {
+        
         var receivedURLs = [URL]()
         
+        /// Everytime we invoke the dataTask(with urL: ), we capture the URL in the receivedURLs array.
+        /// - Parameters:
+        ///   - url: The url we pass into the data task.
+        ///   - completionHandler: The completion handler that is called after the request have completed.
+        /// - Returns: A mock implementation of the dataTask.
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             receivedURLs.append(url)
             return FakeURLSessionDataTask()
