0442a81bb97bf970a68ce6611a2864cf253df49a
Obinna <macmartins081@gmail.com>
2023-09-19 14:26:09 +0400

    Enable code coverage gathering.

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -29,7 +29,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        makeSUT().get(from: url) { _ in}
+        makeSUT().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
