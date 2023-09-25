9e5ccd8368c857d1375212bec775a159130df8be
Obinna <macmartins081@gmail.com>
2023-09-25 23:44:01 +0400

    Added more documentations.

--- EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -7,12 +7,46 @@
 
 import Foundation
 
+/**
+ A data structure representing a feed item.
+ 
+ A `FeedItem` contains information about an item in a feed, such as its unique identifier, description, location, and image URL.
+ 
+ - Parameters:
+ - id: A universally unique identifier (UUID) representing the item.
+ - description: An optional description for the item. It can be `nil` if no description is provided.
+ - location: An optional location string indicating where the item was posted. It can be `nil` if no location is specified.
+ - imageURL: The URL pointing to the image associated with the item.
+ 
+ - Important: The `id` parameter must be a valid UUID.
+ - Note: When `description` or `location` is `nil`, it means the corresponding information is not available.
+ */
 public struct FeedItem: Equatable {
+    /// A universally unique identifier (UUID) representing the item.
     public let id: UUID
+    
+    /// An optional description for the item. It can be `nil` if no description is provided.
     public let description: String?
+    
+    /// An optional location string indicating where the item was posted. It can be `nil` if no location is specified.
     public let location: String?
+    
+    /// The URL pointing to the image associated with the item.
     public let imageURL: URL
     
+    /**
+     Initializes a new `FeedItem` instance.
+     
+     - Parameters:
+     - id: A universally unique identifier (UUID) representing the item.
+     - description: An optional description for the item. It can be `nil` if no description is provided.
+     - location: An optional location string indicating where the item was posted. It can be `nil` if no location is specified.
+     - imageURL: The URL pointing to the image associated with the item.
+     
+     - Important: The `id` parameter must be a valid UUID.
+     
+     - Returns: A new `FeedItem` instance initialized with the provided values.
+     */
     public init(id: UUID, description: String?, location: String?, imageURL: URL) {
         self.id = id
         self.description = description
--- EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/Feed Feature/FeedLoader.swift
@@ -7,11 +7,43 @@
 
 import Foundation
 
+/**
+ An enumeration representing the result of loading a feed.
+ 
+ - `success`: Indicates a successful feed loading operation with an associated array of `FeedItem` instances.
+ - `failure`: Indicates a failure in the feed loading operation with an associated `Error`.
+ 
+ Use this enumeration to convey the outcome of loading a feed in your application.
+ 
+ - Note: To handle the result of a feed loading operation, use the associated values of this enumeration.
+ 
+ */
 public enum LoadFeedResult {
+    /// Indicates a successful feed loading operation with an associated array of `FeedItem` instances.
     case success([FeedItem])
+    
+    /// Indicates a failure in the feed loading operation with an associated `Error`.
     case failure(Error)
 }
 
+/**
+ A protocol defining the contract for a feed loader.
+ 
+ A feed loader is responsible for loading feed data asynchronously and providing the result via a completion closure.
+ 
+ Implement this protocol to create custom feed loading functionality within your application.
+ 
+ */
+
 public protocol FeedLoader {
+    /**
+     Loads feed data asynchronously and provides the result via a completion closure.
+     
+     - Parameter completion: A closure that takes a `LoadFeedResult` as its parameter.
+     - `LoadFeedResult`: The result of the feed loading operation, either success or failure.
+     
+     Implementations of this method should call the `completion` closure when the feed loading operation is complete.
+     
+     */
     func load(completion: @escaping (LoadFeedResult) -> Void)
 }
