aca74d4230fa29305eff20aa9817a5de905fdc3c
obinna <obinna.aguwa@ceridian.com>
2024-01-18 21:24:30 +0400

    Move `FeedStore` to its own file.

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,17 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Obinna Aguwa on 18/01/2024.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
