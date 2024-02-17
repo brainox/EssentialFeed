c34926636c4e289f3129047a59668ea0b444aff5
Obinna <macmartins081@gmail.com>
2024-02-18 02:32:32 +0400

    Move  to its own file

---
+++ EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,42 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Obinna on 18/02/2024.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    /// A universally unique identifier (UUID) representing the item.
+    public let id: UUID
+    
+    /// An optional description for the item. It can be `nil` if no description is provided.
+    public let description: String?
+    
+    /// An optional location string indicating where the item was posted. It can be `nil` if no location is specified.
+    public let location: String?
+    
+    /// The URL pointing to the image associated with the item.
+    public let imageURL: URL
+    
+    /**
+     Initializes a new `FeedItem` instance.
+     
+     - Parameters:
+        - id: A universally unique identifier (UUID) representing the item.
+        - description: An optional description for the item. It can be `nil` if no description is provided.
+        - location: An optional location string indicating where the item was posted. It can be `nil` if no location is specified.
+        - imageURL: The URL pointing to the image associated with the item.
+     
+     - Important: The `id` parameter must be a valid UUID.
+     
+     - Returns: A new `FeedItem` instance initialized with the provided values.
+     */
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
