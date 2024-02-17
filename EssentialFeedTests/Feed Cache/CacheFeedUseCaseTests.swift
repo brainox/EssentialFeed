a330fe180bf09942e20a51b28edf7e98decbf07c
Obinna <macmartins081@gmail.com>
2024-02-18 02:28:13 +0400

    Add  data transfer representation to decouple the items mapper from  data models

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -176,9 +176,9 @@ class CacheFeedUseCaseTests: XCTestCase {
     }
     
     private func uniqueItems() -> (models: [FeedItem], local: [LocalFeedItem]) {
-        let items = [uniqueItem(), uniqueItem()]
-        let localItems = items.map { LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL) }
-        return (items, localItems)
+        let models = [uniqueItem(), uniqueItem()]
+        let localItems = models.map { LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL) }
+        return (models, localItems)
     }
 
     private func anyURL() -> URL {
