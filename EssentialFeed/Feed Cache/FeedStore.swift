54212692d9ca8538d64ed7914630f363806fec72
Obinna <macmartins081@gmail.com>
2024-02-17 21:17:06 +0400

    Add  data transfer representation to decouple storage frameworks from  data models

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -13,5 +13,39 @@ public protocol FeedStore {
 
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
 
-    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+    func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
+
+public struct LocalFeedItem: Equatable {
+    /// A universally unique identifier (UUID) representing the item.
+    public let id: UUID
+    
+    /// An optional description for the item. It can be `nil` if no description is provided.
+    public let description: String?
+    
+    /// An optional location string indicating where the item was posted. It can be `nil` if no location is specified.
+    public let location: String?
+    
+    /// The URL pointing to the image associated with the item.
+    public let imageURL: URL
+    
+    /**
+     Initializes a new `FeedItem` instance.
+     
+     - Parameters:
+        - id: A universally unique identifier (UUID) representing the item.
+        - description: An optional description for the item. It can be `nil` if no description is provided.
+        - location: An optional location string indicating where the item was posted. It can be `nil` if no location is specified.
+        - imageURL: The URL pointing to the image associated with the item.
+     
+     - Important: The `id` parameter must be a valid UUID.
+     
+     - Returns: A new `FeedItem` instance initialized with the provided values.
+     */
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
 }
--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -39,11 +39,12 @@ class CacheFeedUseCaseTests: XCTestCase {
         let timestamp = Date()
         let items = [uniqueItem(), uniqueItem()]
         let (sut, store) = makeSut(currentDate: { timestamp })
+        let localItems = items.map { LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL) }
 
         sut.save(items) { _ in }
         store.completeDeletionSuccessfully()
 
-        XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed, .insert(items, timestamp)])
+        XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed, .insert(localItems, timestamp)])
     }
 
     func test_save_failsOnDeletionError() {
@@ -138,7 +139,7 @@ class CacheFeedUseCaseTests: XCTestCase {
 
         enum ReceivedMessage: Equatable {
             case deleteCachedFeed
-            case insert([FeedItem], Date)
+            case insert([LocalFeedItem], Date)
         }
 
         private(set) var receivedMessages = [ReceivedMessage]()
@@ -159,7 +160,7 @@ class CacheFeedUseCaseTests: XCTestCase {
             deletionCompletions[index](nil)
         }
 
-        func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
+        func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
             insertionCompletions.append(completion)
             receivedMessages.append(.insert(items, timestamp))
         }
